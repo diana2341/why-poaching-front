9e160d9edb116b444ae91a0bc8f208f2b84b35eb
diana2341 <dianaponce2341@gmail.com>
2020-06-05 21:04:15 -0400

    next page slied

--- src/component/Graph.js
+++ src/component/Graph.js
@@ -1,46 +1,54 @@
-import React from 'react'
-import {Bar,Line,Pie} from 'react-chartjs-2'
-import '../AnimalInfo.css';
+import React, { PureComponent } from 'react';
+import {
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,ResponsiveContainer
+} from 'recharts';
 
-class graph extends React.Component{
-    state={
-        chartData:{
-          labels:['Boston','worcester','springfield','lowell','cambridge','new bedford'],
-          datasets:[
-              {
-                  label:'Population',
-                  data:[
-                      617594,
-                      181045,
-                      153060,
-                      106519,
-                      105162,
-                      95072
-                  ],
-                  backgroundColor:[
-                      'rgba(255,99,132,0.6)',
-                      'rgba(54,162,235,0.6)',
-                      'rgba(75,192,192,0.6)',
-                      'rgba(153,102,255,0.6)',
-                      'rgba(255,159,64,0.6)',
-                      'rgba(255,99,132,0.6)',
+const data = [
+    {
+      name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
+    },
+    {
+      name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
+    },
+    {
+      name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
+    },
+    {
+      name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
+    },
+    {
+      name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
+    },
+    {
+      name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
+    },
+    {
+      name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
+    },
+  ];
+  
+  export default class graph extends PureComponent {
+    static jsfiddleUrl = 'https://jsfiddle.net/alidingling/7j5bbbum/';
+  
+    render() {
+      return (
+        <div className="chart"style={{ width: '100%', height: 300 }}>
+          <ResponsiveContainer>
+            <AreaChart
+              data={data}
+              margin={{
+                top: 10, right: 30, left: 0, bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      );
 
-                  ]
-              }
-          ]
-        }
-    }
-render(){
-    return(
-        <div className="chart">
-          <Bar
-                    data={this.state.chartData}
-                    width={70}
-                    height={30}
-                    options={{ maintainAspectRatio: false }}
-            />
-            </div>
-    )
 }
 }
-export default graph
