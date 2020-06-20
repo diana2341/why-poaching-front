d6d4762376c06ed9c1adcde792db950521e0f519
diana2341 <dianaponce2341@gmail.com>
2020-06-20 16:18:38 -0400

    routes fix

--- app.js
+++ app.js
@@ -7,9 +7,3 @@ const port = process.env.PORT || '8080';
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Running on localhost:${port}`));
-var express = require('express');  
-var router = express.Router();
-
-router.get('/', function(req, res, next) {  
-      res.status(200).send("Hi, It works!")  
-}); 
