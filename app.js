3f21d68a429b597994d3acfd04ba3feefea5fa77
diana2341 <dianaponce2341@gmail.com>
2020-06-20 17:13:36 -0400

    erroe messag

--- app.js
+++ app.js
@@ -1,66 +1,16 @@
-// const express = require('express');
-// const http = require('http');
-// const path = require('path');
-// let app = express();
-// app.use(express.static(path.join(__dirname, 'build')));
-// const port = process.env.PORT || '8080';
-// app.set('port', port);
-// const server = http.createServer(app);
-// server.listen(port, () => console.log(`Running on localhost:${port}`));
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// }
-
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-import cluster from 'cluster'
-import express from 'express'
-import os from 'os'
-import path from 'path'
-
-import { app } from '../api/src/app'
-import { logger } from '../api/src/services/logger'
-import { config } from './config'
-
-app.use(express.static(path.join(__dirname, '../client/build')))
-
-app.get('*', (_, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'), (err) => {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
-const startServer = () => {
-  app.listen(+config.http.port, config.http.host, () => {
-    logger.info(`Server started at [ http://${config.http.host}:${config.http.port} ]`)
-    logger.info(`Environment ${process.pid}: ${config.env}`)
-  })
+const express = require('express');
+const http = require('http');
+const path = require('path');
+let app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
 }
 
-const startClusterServer = () => {
-  if (!cluster.isMaster) {
-    return startServer()
-  }
-
-  logger.info(`Master ${process.pid} is running`)
-  const numCPUs = os.cpus().length
-
-  logger.info(`Forking ${numCPUs} clusters`)
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
-
-  cluster.on('exit', (worker) => {
-    logger.info(`worker ${worker.process.pid} died`)
-  })
-}
-
-if (config.nodeClusterEnabled) {
-  startClusterServer()
-} else {
-  startServer()
-}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
