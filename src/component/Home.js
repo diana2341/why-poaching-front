fbd034dfa81dd45feddd35c671a0b66fa03a8326
diana2341 <dianaponce2341@gmail.com>
2020-06-25 22:55:08 -0400

    container

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,15 +6,16 @@ export default class Home extends React.Component{
         return(
             <>
             <img alt=''src="https://i.ya-webdesign.com/images/bird-gif-png-8.gif"/>
-            <div className="home-bg"> 
+            <div className="home-bg"></div> 
+            <div className='box-text-c'>
             <div className="container-text">
                 Endangered Lives
             </div>
 
-            </div>
+            
             <p onClick={()=>{this.props.routerProps.history.push(`/map`)}}className="explore"> Explore Map</p>
             <img alt=''onClick={()=>{this.props.routerProps.history.push(`/map`)}} className="world" src={require("../img/world.png")}/>
-
+            </div>
             </>
         )
     }
