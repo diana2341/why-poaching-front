7925a40ec4664ef5b20f5f9bc4cc617d47192825
diana2341 <dianaponce2341@gmail.com>
2021-05-05 17:18:59 -0400

    menu

--- src/component/MenuPop.js
+++ src/component/MenuPop.js
@@ -2,23 +2,21 @@ import React from 'react'
 import '../MenuPop.css';
 
 import { Link } from 'react-router-dom';
-
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export default class menuPop extends React.Component{
     render(){
        
 
         return(
-            <div className="cir-menu">
-              <div className="thing" ><p className="m"><Link to="/map">Map</Link></p></div>
-
-                <div className="thing"><p className="p"><Link  to="/community">Community</Link></p></div>
-
-                <div className="thing"><p className="po"><Link  to="/poachers">Poachers</Link></p></div>
-
-                <div className="thing"><p className="p-r"><Link  to="/sources">Sources</Link></p></div>
-        
-             </div>
+            <div className="cir-menu"> 
+                <NavDropdown title={<i class="fas fa-bars"></i>} id="nav-dropdown">
+                    <NavDropdown.Item href='/map'eventKey="4.1">Map</NavDropdown.Item>
+                    <NavDropdown.Item href='/community'eventKey="4.2">Community</NavDropdown.Item>
+                    <NavDropdown.Item href='poachers'eventKey="4.3">Poachers</NavDropdown.Item>
+                    <NavDropdown.Item href='/sources'eventKey="4.3">Sources</NavDropdown.Item>
+                </NavDropdown>
+            </div>
         )
     }
 }
